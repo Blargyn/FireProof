07dd90a9f002ce7cdd8dd2d4ecedfd3320340041
Andrew Hawn <blargyn@gmail.com>
2018-12-01 03:47:23 -0800

    Added ItemsPage
    Added ItemsPage functionality and implemented popups for both inventory pages

--- FireProof/FireProof/FireProof/itemsPage.xaml.cs
+++ FireProof/FireProof/FireProof/itemsPage.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Rg.Plugins.Popup.Services;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -15,6 +17,37 @@ namespace FireProof
 		public itemsPage ()
 		{
 			InitializeComponent ();
-		}
-	}
+
+            MessagingCenter.Subscribe<ItemModel>(this, "PopUpItem", (item) => Handle_AddItem(item));
+        }
+
+
+        async void Handle_AddItem(ItemModel item)//, EventArgs e)
+        {
+            await App.Database.SaveItemAsync(item); //should check for duplicates before saving
+
+            //try to update immediately upon adding a room
+            var allItems = await App.Database.GetAllItems();
+
+            var itemList = new ObservableCollection<ItemModel>();
+            allItems.ForEach(x => itemList.Add(x));
+
+            ItemListView.ItemsSource = itemList;
+        }
+
+        private void Handle_AddIPopup(object sender, EventArgs e)
+        {
+            PopupNavigation.Instance.PushAsync(new itemPopup());
+        }
+
+        async void Handle_GetItems(object sender, EventArgs e)
+        {
+            var allItems = await App.Database.GetAllItems();
+
+            var itemList = new ObservableCollection<ItemModel>();
+            allItems.ForEach(x => itemList.Add(x));
+
+            ItemListView.ItemsSource = itemList;
+        }
+    }
 }
--- FireProof/FireProof/FireProof/viewAndManagePage.xaml.cs
+++ FireProof/FireProof/FireProof/viewAndManagePage.xaml.cs
@@ -26,7 +26,7 @@ namespace FireProof
             {
                 roomName = sender,
             };
-            await App.Database.SaveRoomAsync(room);
+            await App.Database.SaveRoomAsync(room); //should check for duplicates before saving
 
             //try to update immediately upon adding a room
             var allRooms = await App.Database.GetAllRooms();
@@ -37,7 +37,7 @@ namespace FireProof
             RoomListView.ItemsSource = roomList;
         }
 
-        private void Handle_AddPopup(object sender, EventArgs e)
+        private void Handle_AddRPopup(object sender, EventArgs e)
         {
             PopupNavigation.Instance.PushAsync(new roomPopup());
         }
@@ -50,6 +50,11 @@ namespace FireProof
             allRooms.ForEach(x => roomList.Add(x));
 
             RoomListView.ItemsSource = roomList;
+        }
+
+        async void Handle_ItemPage(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new itemsPage());
         }
 
 
